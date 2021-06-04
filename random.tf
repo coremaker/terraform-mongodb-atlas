eda4fd87e6994c66f08fa2b2d1f421843bdf46db
Cosmin Briscaru <cosmin@coremaker.io>
2021-06-04 14:03:44 +0100

    add lifecycle block for random resources

--- random.tf
+++ random.tf
@@ -2,9 +2,17 @@ resource "random_string" "mongodb_atlas_cluster_random_name" {
   length = 4
   special = false
   upper = false
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "random_password" "mongodb_atlas_root_user_pass" {
   length = 24
   special = false
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
