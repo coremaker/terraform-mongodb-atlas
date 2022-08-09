307c3b439cefbca6f327434d29ed517660bc8dc1
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-08-09 18:18:10 +0300

    Add pre-commit hooks changes

--- random.tf
+++ random.tf
@@ -1,7 +1,7 @@
 resource "random_string" "mongodb_atlas_cluster_random_name" {
-  length = 4
+  length  = 4
   special = false
-  upper = false
+  upper   = false
 
   lifecycle {
     ignore_changes = all
@@ -9,7 +9,7 @@ resource "random_string" "mongodb_atlas_cluster_random_name" {
 }
 
 resource "random_password" "mongodb_atlas_root_user_pass" {
-  length = 24
+  length  = 24
   special = false
 
   lifecycle {
