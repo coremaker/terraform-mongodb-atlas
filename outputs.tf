ababea198ae3e54f33d3c738e331f6d2c43b7d72
alexandra-buica <109084033+alexandra-buica@users.noreply.github.com>
2022-10-07 12:42:51 +0300

    Add pre-commit changes

--- outputs.tf
+++ outputs.tf
@@ -10,29 +10,29 @@ output "mongodb_atlas_project_id" {
 }
 
 output "mongodb_atlas_mongo_uri" {
-  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider.0.mongo_uri : mongodbatlas_cluster.mongodb_atlas.0.mongo_uri
+  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider[0].mongo_uri : mongodbatlas_cluster.mongodb_atlas[0].mongo_uri
   description = "Base connection string for the cluster"
   sensitive   = true
 }
 
 output "mongodb_atlas_mongo_uri_with_options" {
-  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider.0.mongo_uri_with_options : mongodbatlas_cluster.mongodb_atlas.0.mongo_uri_with_options
+  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider[0].mongo_uri_with_options : mongodbatlas_cluster.mongodb_atlas[0].mongo_uri_with_options
   description = "Includes the replicaSet, ssl, and authSource in the connection string."
   sensitive   = true
 }
 
 output "mongodb_atlas_connection_strings" {
-  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider.0.connection_strings : mongodbatlas_cluster.mongodb_atlas.0.connection_strings
+  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider[0].connection_strings : mongodbatlas_cluster.mongodb_atlas[0].connection_strings
   description = "Set of connection strings that your applications use to connect"
   sensitive   = true
 }
 
 output "mongodb_atlas_cluster_id" {
-  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider.0.cluster_id : mongodbatlas_cluster.mongodb_atlas.0.cluster_id
+  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider[0].cluster_id : mongodbatlas_cluster.mongodb_atlas[0].cluster_id
   description = "Returns the cluster id."
 }
 
 output "mongodb_atlas_cluster_name" {
-  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider.0.name : mongodbatlas_cluster.mongodb_atlas.0.name
+  value       = local.mongodb_atlas_is_tenant ? mongodbatlas_cluster.tenant_provider[0].name : mongodbatlas_cluster.mongodb_atlas[0].name
   description = "Returns the cluster name."
 }
